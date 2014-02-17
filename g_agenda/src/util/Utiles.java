3ae95323fcecf70e44b76bb9d2508a1826501b22
Ferran <ferranpar@yahoo.com>
2014-02-17 14:38:41 +0100

    Clase Utiles

---
+++ g_agenda/src/util/Utiles.java
@@ -0,0 +1,17 @@
+package util;
+
+public class Utiles {
+	
+	
+
+	public int [] Horas_Array(String Horas)
+	{
+		int [] Arry = null;
+		return Arry;			
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
