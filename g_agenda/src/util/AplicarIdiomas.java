35481bab1329fee48a2cf7293bf501f94cee7f69
PereRoura <pererouraperez@gmail.com>
2014-02-18 13:17:33 +0100

    AplicarIdiomas

--- g_agenda/src/util/AplicarIdiomas.java
+++ g_agenda/src/util/AplicarIdiomas.java
@@ -17,15 +17,44 @@ public class AplicarIdiomas extends LeerFichero{
 	
 	private void TratarLista (List <String> lista)
 	{
-		String PrimLinea, dias, iniciales;
+		String PrimLinea, dias, iniciales, tempo, gener, cerr, err;
 		
 		PrimLinea=lista.get(0);
 		dias=lista.get(1);
 		iniciales=lista.get(2);
+		tempo=lista.get(3);
+		gener=lista.get(4);
+		cerr=lista.get(5);
+		err=lista.get(6);
 		
-		System.out.println(dias);
+		
+		Split (String PrimLinea: PrimLinea.split(",",2));
+		
+				String Str = new String();
+			
+				String PrimLinea=lista.get(0);
+				String[] splits = PrimLinea.split(",");
+
+				System.out.println("splits.size: " + splits.length);
+
+				for(String asset: assetClasses){
+				System.out.println(asset);
+				}
+
+
+				
+
+			      System.out.println("dias");
+			      for (String Str1: PrimLinea.split(",", 2)){
+			         System.out.println(dias);
+			      
+		
+		
 		System.out.println(iniciales);
-		
+		System.out.println(tempo);
+		System.out.println(gener);
+		System.out.println(cerr);
+		System.out.println(err);
 		
 				
 	}
