0bd58852257eabba8ba5be080a68006cc1c1eda9
PereRoura <pererouraperez@gmail.com>
2014-02-18 09:42:13 +0100

    Cambios AplicarIdiomas

--- g_agenda/src/util/AplicarIdiomas.java
+++ g_agenda/src/util/AplicarIdiomas.java
@@ -1,11 +1,57 @@
 package util;
 
+import java.io.IOException;
+import java.util.List;
+
+import negocio.LeerFicheroConfig;
+
 public class AplicarIdiomas extends LeerFichero{
-
-	public static void main(String[] args) 
+	
+	
+	public AplicarIdiomas (String fichero) throws IOException
 	{
-		// TODO Auto-generated method stub
-
+		super (fichero);		
+		TratarLista (super.getLista());
 	}
+	
+	
+	private void TratarLista (List <String> lista)
+	{
+		String PrimLinea, dias, iniciales;
+		
+		PrimLinea=lista.get(0);
+		dias=lista.get(1);
+		iniciales=lista.get(2);
+		
+		System.out.println(dias);
+		System.out.println(iniciales);
+		
+		
+				
+	}
+	
+	public static void main(String[] args)
+	{
+		try {
+		
+			
+			AplicarIdiomas idioma = new AplicarIdiomas("internacional.cat");
+			
+				
+			
+			
+	//		PrimLinea=idioma.get(0);
+	//		SegundaLinea=idioma.get(1);
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+	
+
+		
+	}
+	
 
 }
