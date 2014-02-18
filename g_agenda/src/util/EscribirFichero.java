6f9808a447d28288ceee67ad9ea3cf2c7557cee6
Ferran <ferranpar@yahoo.com>
2014-02-18 11:44:39 +0100

    Escribir

--- g_agenda/src/util/EscribirFichero.java
+++ g_agenda/src/util/EscribirFichero.java
@@ -21,44 +21,49 @@ import java.util.List;
 
 import javax.activation.MailcapCommandMap;
 
+	/**
+	 * @author poo
+	 *
+	 */
 	public class EscribirFichero {
 		
 			private List<String> lista = new ArrayList<String>();
 			String nombreArchivo;
-
+			
+			// Método constructor. Hay que pasar una lista con los datos, y el nombre del archivo que quieres generar
 			public  EscribirFichero(List<String> lista, String nombreArchivo) throws IOException{
 				this.lista=lista;
 				this.nombreArchivo=nombreArchivo;
-				//System.out.println(lista);
 				
-				//PrintWriter pw = new PrintWriter(nombreArchivo);
 				
-			PrintWriter pw = new PrintWriter( new BufferedOutputStream(new FileOutputStream(nombreArchivo)),true);				 
-			        				
-				pw.println(lista.get(0));				
-				pw.println(lista.get(1)); 
-				pw.println(lista.get(2));				
-				pw.println(lista.get(3)); 
-				pw.println(lista.get(4)); 
-				    
-		      
-				 
-		        pw.close();
-		   	  	System.out.println("Fichero " + nombreArchivo + " generado.");
-				 
+				
+			// Creamos el fichero. También funciona con **PrintWriter pw = new PrintWriter(nombreArchivo);**
+			PrintWriter fichero = new PrintWriter( new BufferedOutputStream(new FileOutputStream(nombreArchivo)),true);				 
+				
+				// hacemos un bucle para leer el contenido de la lista y 
+				//escribirlo en el fichero por líneas
+				
+					for (int i=0; i<lista.size(); i++)			
+					fichero.println(lista.get(i));	
 		
+				fichero.close();//cerramos el fichero
+		   	  	System.out.println("Fichero " + nombreArchivo + " generado.");
 		}
-			public static void main(String[] args) throws IOException {
+
+			
+			
+			
+			/*			public static void main(String[] args) throws IOException {
 				
 				List<String> listaPepito =				
 						
 						
-						new ArrayList<String>() {{ add("#Resum Activitats 11/2008"); add("#Activitat ReunioC");add("Espai: Sala1 Dia: X 19/11/2008 Hora: 10-11 Conflicte con: ReunioJava");add("Espai: Sala1 Dia: V 21/11/2008 Hora: 10-11 Conflicte con: ReunioJava");add("--------> Total: 10 / 12 h assignades. (No Assignades: 4 h)");}};
+						new ArrayList<String>() {{ add("probando el bucle"); add("#Activitat ReunioC");add("Espai: Sala1 Dia: X 19/11/2008 Hora: 10-11 Conflicte con: ReunioJava");add("Espai: Sala1 Dia: V 21/11/2008 Hora: 10-11 Conflicte con: ReunioJava");add("ultima entrada");}};
 				
 				
 				EscribirFichero fichePepito=new EscribirFichero (listaPepito,"incidencias.log");
 				System.out.println(fichePepito);
-			}
+			}*/
 			
 	}
 
