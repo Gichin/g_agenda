69ab8bf10dd8f93f600169e045eaa27028c4c085
wicka <mesaes@gmail.com>
2014-02-19 13:50:36 +0100

    Clase Utiles el Metodo devolver dia de la semana

--- g_agenda/src/util/Utiles.java
+++ g_agenda/src/util/Utiles.java
@@ -1,12 +1,17 @@
 package util;
 
 import java.io.IOException;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
-public class Utiles {	
-
-	public static int [] Horas_Array(String Hor)  throws Exception
+public class Utiles {		
+	
+	public static int [] Horas_Array(String Hor)  throws Exception  //ESTHER
 	{			
 				
 		String [] Parte = Hor.split("_");		
@@ -40,7 +45,6 @@ public class Utiles {
 		
 		for (int n:iHora1){			
 			Arry[i]=hora1I;			
-			System.out.println(Arry[i]);
 			i++;
 			hora1I++;
 		}
@@ -49,17 +53,57 @@ public class Utiles {
 		
 		for (int n:iHora2){					
 			Arry[i]=hora2I;			
-			System.out.println(Arry[i]);
-			i++;
+				i++;
 			hora2I++;
 		}	
 		return Arry;	
 	}
 	
+	public static char DiadelaSemana(int dia, int mes, int any, AplicarIdiomas Idioma)  throws Exception  //ESTHER
+	{			
+		Calendar ca = Calendar.getInstance();		
+        ca.set(Calendar.DAY_OF_MONTH, dia);
+        ca.set(Calendar.MONTH, mes );
+        ca.set(Calendar.YEAR, any);	    
+        int numDia= ca.get(Calendar.DAY_OF_WEEK);
+        
+        char letra = 0;		
+        
+        switch (numDia) {
+		case 1:		
+			letra=Idioma.getDE();
+			break;
+		case 2:			
+			letra=Idioma.getLE();
+			break;
+		case 3:			
+			letra=Idioma.getME();
+		break;
+		case 4:			
+			letra=Idioma.getXE();
+			break;
+		case 5:			
+			letra=Idioma.getJE();
+			break;
+		case 6:			
+			letra=Idioma.getVE();
+			break;
+		case 7:			
+			letra=Idioma.getSE();
+			break;
+		default:
+			break;
+		}       		
+		return letra;	        
+	}
+	
 	public static void main(String[] args) throws Exception {	
+		
+		AplicarIdiomas rr = new AplicarIdiomas("CAT", "ENG");
 			
 			Horas_Array ("08-12");
 			Horas_Array ("08-11_15-20");					
+			DiadelaSemana(20, 2, 2014, rr);
 }		
 
 }
